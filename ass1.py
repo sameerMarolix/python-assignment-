452c79e9cfff16cdd6a66b074ec95704fe7af710
sameerMarolix <sameermohammad.marolixgmail.com>
2023-12-15 18:36:44 +0530

    conditional statement

---
+++ ass1.py
@@ -0,0 +1,17 @@
+"""Given an integer n, for every integer i <= n, 
+the task is to print “FizzBuzz” if i is divisible by
+ 3 and 5, “Fizz” if i is divisible by 3, “Buzz” if i is divisible by 5 or 
+ i (as a string) if none of the conditions are true"""
+
+
+n = int(input("enter a number: \n"))
+
+if n%3==0 and n%5==0:
+    print("FizzBuzz")
+elif n%3 == 0:
+    print('Fizz')
+elif n%5==0: 
+    print("Buzz")
+else:
+    print(n)
+
